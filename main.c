f40cf8f92b2e0de4db36f353e9968eb23b3f31b8
cprovelengios <cprovelengios@gmail.com>
2022-02-24 14:56:28 +0200

    Init push local files

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "includes.h"
+
+void main(void) 
+{
+    const int delay_ms = 1000;
+    
+    TRISD7 = 0;
+    
+    while (1) 
+    {
+        RD7 = 1;
+        __delay_ms(delay_ms);
+        RD7 = 0;
+        __delay_ms(delay_ms);
+    }
+    
+    return;
+}
